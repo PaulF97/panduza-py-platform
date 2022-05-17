ebe28c4c5bd52e27aeb8abef37e85a2d7feb7a9f
Antoine Gouby <antoine.gouby@protonmail.com>
2022-05-17 20:54:28 +0200

    Added psu fake driver

--- panduza_platform/__init__.py
+++ panduza_platform/__init__.py
@@ -1,5 +1,6 @@
 from .meta_platform import MetaPlatform
 from .meta_driver import MetaDriver
 from .meta_driver_io import MetaDriverIo
+from .meta_driver_psu import MetaDriverPsu
 
 
--- panduza_platform/std_interfaces/__init__.py
+++ panduza_platform/std_interfaces/__init__.py
@@ -1,10 +1,11 @@
 from .driver_fake_io import DriverFakeIo
 from .driver_std_serial import DriverStdSerial
 from .driver_platform import DriverPlatform
+from .driver_psu_fake import DriverPsuFake
 
 PZA_DRIVERS_LIST=[
     DriverPlatform,
     DriverFakeIo,
-    DriverStdSerial
+    DriverStdSerial,
+    DriverPsuFake
 ]
-
