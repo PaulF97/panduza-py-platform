1f39303d59010b3b290238715f656c72f80709de
XRodriguez <xavier.rodriguezalvarez@gmail.com>
2022-06-09 20:44:48 +0200

    Initialize register and reset fake structure

--- package-build.sh
+++ package-build.sh
@@ -1,6 +1,7 @@
+# Build python package
 python3 setup.py sdist bdist_wheel
 
-
+# To upload on pipy
 # twine upload dist/*
 # twine upload -r testpypi dist/*
 
--- panduza_platform/__init__.py
+++ panduza_platform/__init__.py
@@ -3,5 +3,5 @@ from .meta_platform import MetaPlatform
 from .meta_driver import MetaDriver
 from .meta_driver_io import MetaDriverIo
 from .meta_driver_psu import MetaDriverPsu
-
+from .meta_driver_reset import MetaDriverReset
 
